d1430ce6aca36d47580d65cde87d1ef01403161b
ZeCroque <zobilezeub@gmail.com>
2020-11-28 20:12:46 +0100

    wip4

--- server.c
+++ server.c
@@ -41,26 +41,16 @@ int main()
   }
   printf("connexion acceptée!");
   // la connexion est établie, on attend les données envoyées par le client
-  /*nb_octets = read(serviceSocket, &lSize, sizeof(long));
-  printf("taille reçue :%ld\n",lSize);
-  chaine_recue =(char *)malloc(lSize * sizeof(char));
-  nb_octets = read(serviceSocket, chaine_recue, lSize);
-  printf("taille effective:%d", nb_octets);
-  
-  FILE* fichier = NULL;
+  chaine_recue =(char *)malloc(6 * sizeof(char));
+  nb_octets = read(serviceSocket, chaine_recue, 6 * sizeof(char));
+  printf("taille reçue:%d", nb_octets);
+  printf("message:%s",chaine_recue);
 
-  fichier = fopen("reponse.jpg", "wb");
-  int i=0;
-  for(i=0; i<lSize; i++)
-  {
-    fwrite(&chaine_recue[i], 1, sizeof(char), fichier);
-  }
 
-  fclose(fichier);
 
   // on envoie la réponse au client
   write(serviceSocket, reponse, strlen(reponse)+1);
-  */
+
   // on ferme les sockets
   close(serviceSocket);
   close(listenSocket);
