3f084694a53694721bfe5337482557366adbbae0
ZeCroque <zobilezeub@gmail.com>
2020-11-29 17:53:14 +0100

    message_reception

--- server.cpp
+++ server.cpp
@@ -7,8 +7,13 @@
 
 static int listenSocket = 0;
 
-void sigHandler(int sigNo);
-void handleCommunication(int serviceSocket);
+enum class MessageType {HostAdr = 0, ClientAdr = 1, HostCount = 2, ClientConnectionRequest = 3};
+
+struct Message
+{
+	MessageType messageType;
+	struct sockaddr_in adr;
+};
 
 enum class PeerType {None = 0, Client = 1, Host = 2};
 
@@ -18,6 +23,9 @@ struct PeerInfos
 	struct sockaddr_in peerAdr;
 	PeerType peerType;
 };
+
+void sigHandler(int sigNo);
+void handleCommunication(PeerInfos& peerInfos);
 
 int main()
 {
@@ -63,11 +71,7 @@ int main()
 
 		for(auto& peerInfos : peersInfos)
 		{
-			if(peerInfos.peerType == PeerType::None)
-			{
-				std::cout<< inet_ntoa(peerInfos.peerAdr.sin_addr)<<":"<<ntohs(peerInfos.peerAdr.sin_port)<<std::endl;
-				peerInfos.peerType = PeerType::Client;
-			}
+			handleCommunication(peerInfos);
 		}
 			/*handleCommunication(serviceSocket);
 			return 0;*/
@@ -85,21 +89,51 @@ void sigHandler(int sigNo)
 	exit(0);
 }
 
-void handleCommunication(int serviceSocket)
+void handleCommunication(PeerInfos& peerInfos)
 {
-	/*char* inputBuffer;
-	char *outputBuffer = "bien recu";
-	int bufferLength;
-	listenSocket = 0;
+	std::cout<< inet_ntoa(peerInfos.peerAdr.sin_addr)<<":"<<ntohs(peerInfos.peerAdr.sin_port)<<std::endl;
+				
+				//peerInfos.peerType = PeerType::Client;
 	
-	inputBuffer =(char *)malloc(6 * sizeof(char));
-	bufferLength = read(serviceSocket, inputBuffer, 6 * sizeof(char));
-	printf("Received size:%d", bufferLength);
-	printf("Message:%s",inputBuffer);
+	Message message;
+	int receivedSize = read(peerInfos.peerServiceSocket, &message, sizeof(message));
+	if(receivedSize == -1 && errno != EAGAIN)
+	{
+		std::cerr<<"Error: Read error";
+		exit(-1);
+	}
+	if(receivedSize>-1)
+	{
+		std::cout<< "Received message from:"<<inet_ntoa(peerInfos.peerAdr.sin_addr)<<":"<<ntohs(peerInfos.peerAdr.sin_port)<<std::endl;
+		std::cout<<"Received size:"<<receivedSize<<std::endl;
 
+		switch(message.messageType)
+		{
+		case MessageType::HostAdr:
+			peerInfos.peerType = PeerType::Host;
+			std::cout<< "Host waiting for party at:"<<inet_ntoa(message.adr.sin_addr)<<":"<<ntohs(message.adr.sin_port)<<std::endl;
+			break;
+		case MessageType::ClientAdr: 
+			break;
+		case MessageType::HostCount: 
+			break;
+		case MessageType::ClientConnectionRequest:
+			break;
+		default: 
+			std::cerr<<"Error: Unknown message"<<std::endl;
+			exit(-1);
+		}
+		close(serviceSocket);
+	}
+	
+	
+	
+
+	//std::cout<< inet_ntoa(peerInfos.peerAdr.sin_addr)<<":"<<ntohs(peerInfos.peerAdr.sin_port)<<std::endl;
+	
 	// on envoie la réponse au client
-	write(serviceSocket, outputBuffer, strlen(outputBuffer)+1);
+	//write(serviceSocket, outputBuffer, strlen(outputBuffer)+1);
 
 	// on ferme les sockets
-	close(serviceSocket);*/
+	
 }
